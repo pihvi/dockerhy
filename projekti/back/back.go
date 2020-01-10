6c5512d8d14ae46fb20b30af0fd32fa5ce299899
Antti Mattila <72422+pihvi@users.noreply.github.com>
2020-01-11 00:02:19 +0200

    Abuse CPU

--- projekti/back/back.go
+++ projekti/back/back.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 )
 
+var a = 0
+
 func response(rw http.ResponseWriter, request *http.Request) {
+	for i := int64(1); i <= 10000000; i++ {
+		a = rand.Int() / rand.Int()
+	}
+	println(a)
 	rw.Write([]byte("Hello world."))
 }
 
